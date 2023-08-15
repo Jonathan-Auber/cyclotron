55c733b115ad6c93a43a6bd46c6f56595667b2a5
Jonathan.Auber <auber.jonathan@gmail.com>
2023-08-15 17:14:48 +0200

    Modif mineure sur stock

--- config/Database.php
+++ config/Database.php
@@ -9,7 +9,6 @@ class Database
     private static $instance = NULL;
     public static function getPdo(): PDO
     {
-
         if (self::$instance === NULL) {
             self::$instance = new PDO('mysql:host=localhost;dbname=cyclotron;charset=utf8', 'root', 'root', [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
