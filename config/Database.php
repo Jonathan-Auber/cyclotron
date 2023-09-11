6fc8f7367d0d21f422f9d9d41a2bc94d6ad190ae
Jonathan.Auber <auber.jonathan@gmail.com>
2023-09-11 13:43:18 +0200

    Retrait de la bdd de la version web

--- config/Database.php
+++ config/Database.php
@@ -9,11 +9,6 @@ class Database
     private static $instance = NULL;
     public static function getPdo(): PDO
     {
-        // if (self::$instance === NULL) {
-        //     self::$instance = new PDO('mysql:host=mysql-jonathan-auber.alwaysdata.net;dbname=jonathan-auber_cyclotron;charset=utf8', '304530', 'the_net_warrior', [
-        //         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        //         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        //     ]);
         if (self::$instance === NULL) {
             self::$instance = new PDO('mysql:host=localhost;dbname=cyclotron;charset=utf8', 'root', 'root', [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
